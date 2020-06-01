759d581f0ab1f5d579bd1d4deed2a973ba3744d2
Mert Bozkurt <imertbozkurt@gmail.clom>
2020-06-01 16:41:55 +0300

    Grouped by components (BmiController &BmiResult)

--- src/App.tsx
+++ src/App.tsx
@@ -1,105 +1,94 @@
-import React,{useRef, useState} from 'react';
-import { IonApp, IonHeader, IonToolbar, IonContent, IonTitle, IonGrid, IonRow, IonCol, IonItem, IonLabel, IonInput, IonButton, IonIcon, IonCard, IonCardContent } from '@ionic/react';
-import Home from './pages/Home';
-import {calculatorOutline,refreshOutline} from 'ionicons/icons'
+import React, { useRef, useState } from "react";
+import {
+  IonApp,
+  IonHeader,
+  IonToolbar,
+  IonContent,
+  IonTitle,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonItem,
+  IonLabel,
+  IonInput,
+} from "@ionic/react";
+import Home from "./pages/Home";
+
+import BmiControls from "./components/BmiControls";
+import BmiResults from "./components/BmiResults";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
 //React.FC => React functional component
 const App: React.FC = () => {
-  const [calculatedBmi,setCalculatedBmi] = useState<number>();
+  const [calculatedBmi, setCalculatedBmi] = useState<number>();
 
   const weightInputRef = useRef<HTMLIonInputElement>(null);
   const heightInputRef = useRef<HTMLIonInputElement>(null);
 
-  const calculateBMI = () =>{
+  const calculateBMI = () => {
     const enteredWeight = weightInputRef.current!.value;
     const enteredHeight = heightInputRef.current!.value;
-    if(!enteredHeight || !enteredWeight){
+    if (!enteredHeight || !enteredWeight) {
       return;
     }
-    const bmi = +enteredWeight / (+enteredHeight * +enteredHeight)
+    const bmi = +enteredWeight / (+enteredHeight * +enteredHeight);
     console.log(bmi);
     setCalculatedBmi(bmi);
   };
-  const resetInputs = () =>{
-    weightInputRef.current!.value ='';
-    heightInputRef.current!.value ='';
+  const resetInputs = () => {
+    weightInputRef.current!.value = "";
+    heightInputRef.current!.value = "";
   };
 
-
   return (
-  <IonApp>
-    <IonHeader>
-      <IonToolbar >
-       <IonTitle>BMI Calculator</IonTitle> 
-      </IonToolbar>
-    </IonHeader>
-    <IonContent className="ion-padding">
-      <IonGrid>
-        <IonRow>
-          <IonCol>
-            <IonItem>
-              <IonLabel position="floating">Your Height</IonLabel>
-              <IonInput ref={heightInputRef}></IonInput>
-            </IonItem>
-          </IonCol>
-        </IonRow>
-        <IonRow>
-          <IonCol>
-            <IonItem>
-              <IonLabel position="floating">Your Weight</IonLabel>
-              <IonInput ref={weightInputRef}></IonInput>
-            </IonItem>
-          </IonCol>
-        </IonRow>
-        <IonRow>
-          <IonCol className="ion-text-left">
-            <IonButton onClick={calculateBMI}>
-              <IonIcon slot="start" icon={calculatorOutline}/>
-              Calculate
-            </IonButton>
-          </IonCol>
-          <IonCol className="ion-text-right">
-            <IonButton onClick={resetInputs}>
-              <IonIcon slot="start" icon={refreshOutline} />
-              Reset
-            </IonButton>
-          </IonCol>
-        </IonRow>
-        {calculatedBmi &&(
-        <IonRow>
-          <IonCol>
-            <IonCard>
-              <IonCardContent>
-                <h2>{calculatedBmi}</h2>
-              </IonCardContent>
-            </IonCard>
-            
-          </IonCol>
-        </IonRow>)}
-      </IonGrid>
-
-    </IonContent>
-    
-  </IonApp>);
+    <IonApp>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>BMI Calculator</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="floating">Your Height</IonLabel>
+                <IonInput ref={heightInputRef}></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonItem>
+                <IonLabel position="floating">Your Weight</IonLabel>
+                <IonInput ref={weightInputRef}></IonInput>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <BmiControls onCalculate={calculateBMI} onReset={resetInputs} />
+          {calculatedBmi && <BmiResults result={calculatedBmi} />}
+        </IonGrid>
+      </IonContent>
+    </IonApp>
+  );
 };
 
 export default App;
---
+++ src/components/BmiResults.tsx
@@ -0,0 +1,21 @@
+import React  from 'react';
+
+import {IonRow,IonCol,IonCard,IonCardContent} from '@ionic/react';
+
+const BmiResult: React.FC<{result: number | string}> = props => {
+    return (
+    <IonRow>
+        <IonCol>
+        <IonCard>
+            <IonCardContent>
+            <h2>{props.result}</h2>
+            </IonCardContent>
+        </IonCard> 
+        </IonCol>
+    </IonRow>
+    );
+};
+
+
+
+export default BmiResult;
